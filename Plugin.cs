5378b15fcc8b5fc643d17293cf230f142d09654e
Eva <17984234+idijt@users.noreply.github.com>
2022-02-10 00:51:28 +0200

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace NoLevelLoss
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [HarmonyPatch]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new Harmony(PluginInfo.PLUGIN_GUID).PatchAll();
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Skills), "OnDeath")]
+        static bool Skills_OnDeath()
+        {
+            return false;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# NoLevelLoss
+
+Prevents you from losing levels when dying in Valheim.
+
+## Setting up
+
+You need [.NET SDK 6](https://dotnet.microsoft.com/) or newer
+
+1. populate `lib` folder with `assembly_valheim.dll`
+   - this can be found in the game's `Valheim_Data/Managed`
+2. run `dotnet restore`
+
+## Building
+
+1. run `dotnet build`
+2. The output can be found in `bin/Debug/net46`
